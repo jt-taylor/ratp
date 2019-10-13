d36a21ae8a8baee5645ffcc76b1aa21ae1ed4f25
Tigran Katolikyan <tkatolik@e1z2r3p7.42.us.org>
2019-10-13 14:46:38 -0700

    js to classes update

--- js-to-class.py
+++ js-to-class.py
@@ -4,27 +4,34 @@ class stop_area:
     def __init__(self, name, stop_id):
         self.name = name
         self.stop_id = stop_id
-       # self.physical_mode = physical_mode
-    #def update_time_table(self, time_table):
-    #   self.time_table = time_table
 
 class route:
     def __init__(self, route_id, nodes):
         self.route_id = route_id
         self.nodes = nodes
 
-with open("./tmp_semi_parsed_stop_points.json") as json_file:
-    json_data = json.load(json_file)
-print("List of all routes")
+def get_list_classes_routes(path):
+    with open(path) as json_file:
+        json_data = json.load(json_file)
 
-stops = []
-routes = []
-for line in json_data:
     stops = []
-    for stop in json_data[str(line)]:
-        new_stop = stop_area(stop["label"], stop["id"])
-        stops.append(new_stop)
-    routes.append(route(str(line), stops.copy()))
+    routes = []
+    for line in json_data:
+        stops = []
+        for stop in json_data[str(line)]:
+            new_stop = stop_area(stop["label"], stop["id"])
+            stops.append(new_stop)
+        routes.append(route(str(line), stops.copy()))
+    return (routes)
 
-#print(routes);
-#print(routes[0].nodes[0].name);
+def get_single_class(path):
+    with open(path) as json_file:
+        json_data = json.load(json_file)
+
+    for stop in json_data:
+        new_stop = stop_area(stop["label"], stop["id"])
+    return (new_stop)
+
+#print(get_list_classes_routes("./tmp_semi_parsed_stop_points.json"));
+#print(get_list_classes_routes("./tmp_semi_parsed_stop_points.json")[0].nodes[0].name);
+#print(get_single_class("./tmp_start.json").stop_id);
